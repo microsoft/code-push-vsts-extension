89e387cf0b11815cfc3aa7422307a417156ccab6
Geoffrey Goh <kogoh@microsoft.com>
2015-11-02 16:08:31 -0800

    first-commit

---
+++ CodePush.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var tl = require('vso-task-lib');
+var CodePushCommandExecutor = require("code-push-cli/script/command-executor");
+var CodePushSdk = require("code-push");
+var CodePushCommandTypes = require("code-push-cli/definitions/cli").CommandType;
+
+// User inputs
+var accessKey       = tl.getInput("accessKey", true);
+var appName         = tl.getInput("appName", true);
+var packagePath     = tl.getPathInput("packagePath", true);
+var appStoreVersion = tl.getInput("appStoreVersion", true);
+var deploymentName  = tl.getInput("deploymentName", false);
+var description     = tl.getInput("description", false);
+var isMandatory     = tl.getInput("isMandatory", false);
+
+// Constants
+var SERVER_URL = "https://codepush.azurewebsites.net";
+
+CodePushCommandExecutor.loginWithAccessToken = function () {
+    CodePushCommandExecutor.sdk = new CodePushSdk.AccountManager(SERVER_URL);
+    return CodePushCommandExecutor.sdk.loginWithAccessToken(accessKey);
+};
+
+CodePushCommandExecutor.execute({
+	type: CodePushCommandTypes.release,
+    appName: appName,
+    deploymentName: deploymentName,
+    description: description,
+    mandatory: isMandatory,
+    appStoreVersion: appStoreVersion,
+    package: packagePath
+}).catch(function(error) {
+    console.error(error);
+    tl.exit(0);
+}).done();
---
+++ CodePush.ps1
@@ -0,0 +1,19 @@
+param (
+    [string]$accessKey,
+    [string]$appName,
+    [string]$packagePath,
+    [string]$appStoreVersion,
+    [string]$deploymentName,
+    [string]$description,
+    [string]$isMandatory
+) 
+  
+$env:INPUT_accessKey = $accessKey
+$env:INPUT_appName = $appName
+$env:INPUT_packagePath = $packagePath
+$env:INPUT_appStoreVersion = $appStoreVersion
+$env:INPUT_deploymentName = $deploymentName
+$env:INPUT_description = $description
+$env:INPUT_isMandatory = $isMandatory
+
+node CodePush.js
