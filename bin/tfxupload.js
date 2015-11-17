945341c1650eb06136aecfe87b55ab42939856ca
Geoffrey Goh <kg422@cornell.edu>
2015-11-16 20:50:01 -0800

    formatting

--- bin/tfxupload.js
+++ bin/tfxupload.js
@@ -4,42 +4,42 @@
 */
 
 var path = require("path"),
-	fs = require("fs"),
-	Q = require ("q"),
-	exec = Q.nfbind(require("child_process").exec);
+  fs = require("fs"),
+  Q = require ("q"),
+  exec = Q.nfbind(require("child_process").exec);
 
 function installTasks() {
-	var promise = Q();
-	var taskPath = path.join(process.cwd(), "code-push-vsts-task");
-	promise = promise.then(function() {
-			console.log("Uploading task code-push-vsts-task");
-			process.chdir(taskPath);
-			return npmInstall();
-		})
-		.then(tfxUpload);
-	return promise;
+  var promise = Q();
+  var taskPath = path.join(process.cwd(), "code-push-vsts-task");
+  promise = promise.then(function() {
+      console.log("Uploading task code-push-vsts-task");
+      process.chdir(taskPath);
+      return npmInstall();
+    })
+    .then(tfxUpload);
+  return promise;
 }
 
 function npmInstall() {
-	console.log("Installing npm dependencies for task...");
-	return exec("npm install --only=prod").then(logExecReturn);
+  console.log("Installing npm dependencies for task...");
+  return exec("npm install --only=prod").then(logExecReturn);
 }
 
 function tfxUpload() {
-	console.log("Transferring...")
-	return exec("tfx build tasks upload . --overwrite true").then(logExecReturn);
+  console.log("Transferring...")
+  return exec("tfx build tasks upload . --overwrite true").then(logExecReturn);
 }
 
 function logExecReturn(result) {
-    console.log(result[0]);
-    if (result[1] !== "") {
-        console.error(result[1]);
-    }
+  console.log(result[0]);
+  if (result[1] !== "") {
+    console.error(result[1]);
+  }
 }
 
 installTasks()
-	.done(function() {
-		console.log("Upload complete!");
-	}, function(input) {
-		console.log("Upload failed!");
-	});
+  .done(function() {
+    console.log("Upload complete!");
+  }, function(input) {
+    console.log("Upload failed!");
+  });
