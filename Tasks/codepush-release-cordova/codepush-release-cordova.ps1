1cb731128a537681f1379304d16c80c7d59518fd
Ryu Yu <ryuyu@microsoft.com>
2016-04-21 15:13:02 -0700

    adding base for release-cordova task

---
+++ Tasks/codepush-release-cordova/codepush-release-cordova.ps1
@@ -0,0 +1,29 @@
+param (
+    [string]$authType,
+    [string]$accessKey,
+    [string]$serviceEndpointCodePush,
+    [string]$serviceEndpointHockeyApp,
+    [string]$appName,
+    [string]$deploymentName,
+    [string]$packagePath,
+    [string]$appStoreVersion,
+    [string]$rollout,
+    [string]$description,
+    [string]$isMandatory,
+    [string]$isDisabled
+) 
+  
+$env:INPUT_authType = $authType
+$env:INPUT_accessKey = $accessKey
+$env:INPUT_serviceEndpointCodePush = $serviceEndpointCodePush
+$env:INPUT_serviceEndpointHockeyApp = $serviceEndpointHockeyApp
+$env:INPUT_appName = $appName
+$env:INPUT_deploymentName = $deploymentName
+$env:INPUT_packagePath = $packagePath
+$env:INPUT_appStoreVersion = $appStoreVersion
+$env:INPUT_rolloutput = $rollout
+$env:INPUT_description = $description
+$env:INPUT_isMandatory = $isMandatory
+$env:INPUT_isDisabled = $isDisabled
+
+node codepush-release.js
