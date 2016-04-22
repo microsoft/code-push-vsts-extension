68501b840441cfcd56f25ca84fae8a824f28d4bc
Ryu Yu <ryuyu@microsoft.com>
2016-04-22 15:16:09 -0700

    adding base files for patch task

---
+++ Tasks/codepush-patch/codepush-patch.js
@@ -0,0 +1,120 @@
+var path = require("path");
+var tl = require("vsts-task-lib");
+require("shelljs/global");
+
+// Global variables.
+var codePushCommandPrefix = "node " + path.join(__dirname, "node_modules", "code-push-cli", "script", "cli");
+
+// Export for unit testing.
+function log(message) {
+  console.log(message);
+}
+
+// Helper functions.
+function buildCommand(cmd, positionArgs, optionFlags) {
+  var command = codePushCommandPrefix + " " + cmd;
+  
+  positionArgs && positionArgs.forEach(function(positionArg) {
+    command = command + " \"" + positionArg + "\"";
+  });
+  
+  for (var flag in optionFlags) {
+    // If the value is falsey, the option flag doesn't have to be specified.
+    if (optionFlags[flag]) {
+      var flagValue = "" + optionFlags[flag];
+      
+      command = command + " --" + flag;
+      // For boolean flags, the presence of the flag is enough to indicate its value.
+      if (flagValue != "true" && flagValue != "false") {
+        command = command + " \"" + flagValue + "\"";
+      }
+    }
+  }
+
+  return command;
+}
+
+function executeCommandAndHandleResult(cmd, positionArgs, optionFlags) {
+  var command = buildCommand(cmd, positionArgs, optionFlags);
+  
+  var result = exec(command, { silent: true });
+  
+  if (result.code == 0) {
+    module.exports.log(result.output);
+  } else {
+    tl.setResult(1, result.output);
+    ensureLoggedOut();
+    throw new Error(result.output);
+  }
+  
+  return result;
+}
+
+function ensureLoggedOut() {
+  exec(buildCommand("logout"), { silent: true });
+}
+
+// The main function to be executed.
+function performPatchTask(accessKey, appName, packagePath, appStoreVersion, deploymentName, description, rollout, isMandatory, isDisabled, targetBinaryVersion, label) {
+  // If function arguments are provided (e.g. during test), use those, else, get user inputs provided by VSTS.
+  var authType = tl.getInput("authType", false);
+  if (authType === "AccessKey") {
+      accessKey = tl.getInput("accessKey", true);
+  } else if (authType === "ServiceEndpointCodePush" || authType === "ServiceEndpointHockeyApp") {
+      var serviceAccount = tl.getEndpointAuthorization(tl.getInput(authType, true));
+      accessKey = serviceAccount.parameters.password;
+  }
+
+  appName             = appName || tl.getInput("appName", true);
+  packagePath         = packagePath || tl.getPathInput("packagePath", true);
+  appStoreVersion     = appStoreVersion || tl.getInput("appStoreVersion", true);
+  deploymentName      = deploymentName || tl.getInput("deploymentName", false);
+  description         = description || tl.getInput("description", false);
+  rollout             = rollout || tl.getInput("rollout", false);
+  isMandatory         = isMandatory || tl.getBoolInput("isMandatory", false);
+  isDisabled          = isDisabled || tl.getBoolInput("isDisabled", false);
+  targetBinaryVersion = targetBinaryVersion || tl.getInput("targetBinaryVersion", false);
+  label               = label || tl.getInput("releaseLabel", false);
+  
+  if (!accessKey) {
+      console.error("Access key required");
+      tl.setResult(1, "Access key required");
+  }
+  
+  // Ensure all other users are logged out.
+  ensureLoggedOut();
+  
+  // Log in to the CodePush CLI.
+  executeCommandAndHandleResult("login", /*positionArgs*/ null, { accessKey: accessKey });
+  
+  // Run release command.
+  executeCommandAndHandleResult(
+    "patch", 
+    [appName, deploymentName], 
+    { 
+      targetBinaryVersion: appStoreVersion,
+      label: deploymentName,
+      description: description,
+      rollout: rollout,
+      mandatory: isMandatory,
+      disabled: isDisabled
+    }
+  );
+  
+  // Log out.
+  ensureLoggedOut();
+}
+
+module.exports = {
+  buildCommand: buildCommand,
+  commandPrefix: codePushCommandPrefix,
+  ensureLoggedOut: ensureLoggedOut,
+  executeCommandAndHandleResult: executeCommandAndHandleResult,
+  log: log,
+  performPatchTask: performPatchTask
+}
+
+if (require.main === module) {
+  // Only run the patch task if the script is being run directly, and not imported as a module (eg. during test)
+  performPatchTask();
+}
